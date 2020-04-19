564a513aa730b6d2656a080e2ab951a99585218d
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2020-04-20 04:52:05 +0800

    Update kjzhanFakeUA.user.js

--- kjzhanFakeUA.user.js
+++ kjzhanFakeUA.user.js
@@ -1,25 +1,26 @@
 // ==UserScript==
 // @name         直接显示课件站下载链接
 // @namespace    https://github.com/Rongronggg9/kjzhanFakeUA
-// @version      1.0
+// @version      1.1
 // @description  无需微信或 2345 浏览器以及验证码，直接显示下载链接
 // @author       Rongronggg9
-// @match        *://www.kjzhan.com/*
+// @match        *://www.kjzhan.com/plus/download.php*
 // @run-at       document-start
 // @grant        none
+// @supportURL   https://github.com/Rongronggg9/kjzhanFakeUA/issues
+// @icon         http://www.kjzhan.com/favicon.ico
 // ==/UserScript==
 
 
 (function() {
 	'use strict';
 	navigator.__defineGetter__('userAgent', function() {
-		return 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.108 Safari/537.36 WangPai 2345Explorer/8.4.1.14998';
+		return 'WangPai MicroMessenger';
 	});
-	//匹配的字段是 "WangPai" , 以上来自该网站三年前提供的 2345加速浏览器v8.4 的 UA
 
-    window.addEventListener('load', function() {
-        document.getElementById("ys").style.display = "none";
-        document.getElementById("dow").style.display = "block";
-    }, false);
+	window.addEventListener('load', function() {
+		document.getElementById("ys").style.display = "none";
+		document.getElementById("dow").style.display = "block";
+	}, false);
 
 })();
